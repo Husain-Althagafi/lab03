3b136cef43eb89b21624a41c55fcad39dc173e10
amussab <144953031+amussab@users.noreply.github.com>
2024-09-19 16:23:28 +0300

    whatever

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ class Main{
         System.out.print("Hello World");
         int numStudents_ = 0;
         System.out.print(numStudents_);
+        System.out.println("Shawarma"); //comment
     }
 
     public int numStudents(){
